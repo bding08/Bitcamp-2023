f7aba7c654e79a8fe0977260724c8299e3fc4b1a
Brian Ding <bding08@gmail.com>
2023-04-08 13:18:01 -0400

    cleaned up SentimentCalculator file and added comments

--- bitcamp-server/RedditScraper/SentimentCalculator.py
+++ bitcamp-server/RedditScraper/SentimentCalculator.py
@@ -6,6 +6,9 @@ from nltk.sentiment import SentimentAnalyzer
 from nltk.sentiment.vader import SentimentIntensityAnalyzer
 from nltk.sentiment.util import *
 
+# when running on your own laptop, uncomment the following below to download
+# necessary dependencies and NLTK libraries: 
+
 # nltk.download([
 #      "names",
 #      "stopwords",
@@ -16,26 +19,21 @@ from nltk.sentiment.util import *
 #      "vader_lexicon",
 #      "punkt",
 # ])
+
+# example of tokenizing a string: 
+
 # sentence = "At eight o'clock on Thursday morning Arthur didn't feel very good."
-
 # tokens = nltk.word_tokenize(sentence)
-
 # print("tokens: " + str(tokens))
 
 sia = SentimentIntensityAnalyzer()
-# print(sia.polarity_scores("sachin really sucks but he is alright at coding"))
-# print(sia.__dict__)
 
 df = pd.read_csv("Top Posts.csv")
 print(df)
 
 for index, row in df.iterrows():
     polarity_score = sia.polarity_scores(row['Title'])
-    # print("row " + str(index) + ": " + str(polarity_score))
     print("row " + str(index) + ": " + " Sentiment Analysis Compound Score: " + str(polarity_score['compound']))
-    # print(f"row {index}: {row['name']} is {row['age']} years old and {row['gender']}.")
 
-# vader = SentimentIntensityAnalyzer()
 
-# f = lambda title: vader.polarity_scores(title)['compound']
 
