2ba696f3ab7a5577574fd90dc27ae0f80b46082b
Brian Ding <bding08@gmail.com>
2023-04-10 00:18:16 -0400

    hopefully this fixed errors

--- bitcamp-project/pymongo_get_database.py
+++ bitcamp-project/pymongo_get_database.py
@@ -23,20 +23,23 @@ def insert_into_database(JSON_obj):
 import pymongo
 
 def get_data_from_mongodb():
+    data = []
     # Create a MongoClient object
     connection_string = "mongodb+srv://bitcamp-2023:zfGBxA65Bku2aCPr@bitcamp-2023.0bp6oqs.mongodb.net/test"
     client = pymongo.MongoClient(connection_string)
 
-    # Get the database
+    # # Get the database
     db = client["mydatabase"]
 
-    # Get the collection
+    # # Get the collection
     collection = db["stocks"]
+    print("collection: " + str(collection))
 
-    # Retrieve data from the collection based on the query
-    data = collection.find()
+    # # Retrieve data from the collection based on the query
+    # data = collection.find()
 
     return data
 
 
 
+get_data_from_mongodb()
